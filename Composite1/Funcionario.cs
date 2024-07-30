e7315d37a9c04f92ebd2d90c8aca8a1802f3cc0b
Diego Moraes dos Santos Silva <diego.moraes@grupoaguasdobrasil.com.br>
2024-07-30 18:35:26 -0300

    Adicionar arquivos de projeto.

---
+++ Composite1/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace Composite1
+{
+    //Leaf
+    public class Funcionario : HoraTrabalhada
+    {
+        public int Horas { get; set; }
+        public override int GetHoraTrabalhada()
+        {
+            //logica
+            System.Console.WriteLine($"O funcionário {Nome} registrou {Horas} trabalhadas");
+            return Horas;
+        }
+    }
+}
